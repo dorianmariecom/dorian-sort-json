02ea8a370ba664b981d4f59a21e4d3e27d68d42f
Dorian Marié <dorian@dorianmarie.com>
2024-04-04 01:54:19 +0200

    linting

--- spec/sort_json_spec.rb
+++ spec/sort_json_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 RSpec.describe "sort-json" do
   it "works" do
     expect(`bin/sort-json '{"b": 2, "a": 1}'`).to eq(<<~JSON)
-    {
-      "a": 1,
-      "b": 2
-    }
+      {
+        "a": 1,
+        "b": 2
+      }
     JSON
   end
 end
